064a35994cdbacc7dd7528c4ba86d530d2f3ea7b
Francisco Manuel <fsaa93@gmail.com>
2023-07-25 09:50:51 +0200

    First commit with Backend and Frontend install
    Installing SpringBoot project with Spring Initializr as backend and Angular on frontend

---
+++ backend/lifequest/src/main/java/f5/frasko/LifequestApplication.java
@@ -0,0 +1,13 @@
+package f5.frasko.lifequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifequestApplication.class, args);
+	}
+
+}
---
+++ backend/lifequest/src/test/java/f5/frasko/lifequest/LifequestApplicationTests.java
@@ -0,0 +1,13 @@
+package f5.frasko.lifequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
